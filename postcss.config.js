94dea68a39a0db9568443cdc62597e5168d6bfbd
Peter Davis <peter.w.davis@gmail.com>
2022-09-18 08:51:43 +0930

    Update workflow build

--- postcss.config.js
+++ postcss.config.js
@@ -4,6 +4,8 @@ module.exports = {
     require("tailwindcss"),
     require("autoprefixer"),
     require("postcss-nested"),
-    ...(process.env.NODE_ENV == "production" ? [require("cssnano")({ preset: "default" })] : []),
+    ...(process.env.NODE_ENV == "production" 
+      ? [require("cssnano")({ preset: "default" })] 
+      : []),
   ],
 };
