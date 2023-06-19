9d1d24eac2e152fb81ab2d49b7a614b17ac41a4d
Peter Davis <peter.w.davis@gmail.com>
2023-06-19 12:00:32 +0000

    Fix Tailwind 3.0 upgrade

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,9 @@
 module.exports = {
-  plugins: [
-    require("postcss-import"),
-    require("tailwindcss/nesting"),
-    require("tailwindcss"),
-    require("autoprefixer"),
-    require("postcss-nested"),
-    ...(process.env.NODE_ENV == "production" 
-      ? [require("cssnano")({ preset: "default" })] 
-      : []),
-  ],
+  plugins: {    
+    'postcss-import': {},
+    'tailwindcss/nesting': {},
+    tailwindcss: {},
+    autoprefixer: {},
+    ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {})
+  }
 };
